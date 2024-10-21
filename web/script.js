778d5e3e5df25e22b16b7a29850fb8f3f3a3caf4
Ming-Jung YU <minayu416@gmail.com>
2024-10-21 16:30:47 +0800

    write logics comment

--- web/script.js
+++ web/script.js
@@ -1,8 +1,11 @@
 
+// 通常要取出到constant，但有點懶 :p
+// API 的網址
 // const requestURL = 'http://127.0.0.1:8000/api/predictor/'
 const requestURL = 'https://playground-eight-psi.vercel.app/api/predictor/'
 
 
+// 有多少馬匹跟他們的固定數值。
 const horses = {
   '1': [3, 4],
   '2': [1, 2, 6],
@@ -16,7 +19,7 @@ const allHorse = Array.from({ length: Object.keys(horses).length }, (v, i) => i 
 
 const requestBodyIndex = ["一號", "二號", "三號", "四號"]
 
-
+// id: horseList, 根據有幾個馬匹生成跟渲染 3 cols 的內容。
 function createHorseCompetitors() {
 
   const horseList = document.getElementById('horseList');
@@ -30,20 +33,17 @@ function createHorseCompetitors() {
 
 }
 
+// id: raceList, 生產四個賽道、指定的出賽馬匹、六個道具、一個押金
 function createPlayers() {
   const container = document.getElementById("raceList");
 
+  // For loop 四個賽代
   for (let i = 1; i <= 4; i++) {
-    // 創建外層 div
+    // 創建最外層 div，用display flex 屬性讓 指定的出賽馬匹、六個道具、一個押金 成一列。
     const flexDiv = document.createElement("div");
     flexDiv.className = "flex justify-center gap-35 text-base";
 
-    // 創建第1個 input 和 p 標籤
-    // const p = document.createElement("p");
-    // p.textContent = i;
-    // p.className = "horse-background font-coffee-color competitor-label-padding border-color";
-
-    // 創建第一個 flex-stretch 包含的多個 input
+    // 第一個 div 是出賽馬匹，用flex 包是因為我需要將馬匹的選擇方塊的高度縮小後，將他垂直置中，我不希望他滿版。
     const input1Div = document.createElement("div");
     input1Div.className = "flex flex-align-center";
 
@@ -51,6 +51,7 @@ function createPlayers() {
     select.id = `select-${i}`;
     select.className = "horse-background font-coffee-color competitor-label-padding border-color text-sm"
 
+    // 沒做到拖拉的功能，直接用下拉式選單代替 :p，因此根據有幾匹馬來渲染幾個選項。
     allHorse.forEach((horse, index) => {
       const option = document.createElement("option");
       option.value = horse;
@@ -62,22 +63,15 @@ function createPlayers() {
 
       select.appendChild(option);
     });
-
-    // const input1 = document.createElement("input");
-    // input1.type = "number";
-    // input1.min = 0;
-    // input1.max = 10;
-    // input1.id = `input-${i}-1`;
-    // input1.className = "h-50 border-color text-center text-sm heading-color";
-    
     
     input1Div.appendChild(select);
 
-    // 創建第一個 flex-stretch 包含的多個 input
+    // 第二個div是要包含六個道具，也是用flex 去包住六個道具的 input
     const stretchDiv1 = document.createElement("div");
     stretchDiv1.className = "flex flex-align-center";
 
     for (let j = 2; j <= 7; j++) {
+      // 一個道具輸入的div (包含輸入框跟加減按鈕)
       const numberInputDiv = document.createElement("div");
       numberInputDiv.className = "number-input"
 
@@ -97,6 +91,7 @@ function createPlayers() {
         input.stepUp(); // 增加數字
       };
 
+      // 創建輸入數字的文字框
       const input = document.createElement("input");
       input.type = "number";
       input.value = 0;
@@ -111,10 +106,11 @@ function createPlayers() {
       stretchDiv1.appendChild(numberInputDiv);
     }
 
-    // 創建第二個 flex-stretch 包含1個 input
+    // 創建最後一個是押金的div，一樣用flex 包，也是因為我需要輸入的方塊高度縮小後，將他垂直置中，我不希望他滿版。
     const stretchDiv2 = document.createElement("div");
     stretchDiv2.className = "flex flex-align-center";
 
+    // 輸入金額的文字框
     const input2 = document.createElement("input");
     input2.type = "number";
     input2.min = 0;
@@ -125,12 +121,11 @@ function createPlayers() {
     stretchDiv2.appendChild(input2);
 
     // 將所有子元素加入外層 div: 賽道、出賽的馬匹、道具、押金
-    // flexDiv.appendChild(p);
     flexDiv.appendChild(input1Div);
     flexDiv.appendChild(stretchDiv1);
     flexDiv.appendChild(stretchDiv2);
 
-    // 將外層 div 加入 container
+    // 將外層 div 加入 raceList
     container.appendChild(flexDiv);
   }
 }
@@ -171,6 +166,10 @@ function createPlayers() {
   const submitBtn = document.getElementById('submitBtn');
   const resultDiv = document.getElementById('result');
 
+  // id: submitBtn
+  // 1. 按下submitBtn按鈕後會先透過函式 getInputValuesAsBody 收集所有input的資料。
+  // 2. 接著透過fetch 搭配 async call API
+  // 3. 取得response data 後 id: result div 渲染預測內容。
   submitBtn.addEventListener('click', async () => {
 
     const requestBody = getInputValuesAsBody();
